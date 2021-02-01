c2f7610b8828cec7b263010f371ecd77f39bb5f9
Najamdemo <76806305+Najamdemo@users.noreply.github.com>
2021-02-01 22:17:31 +0500

    responsive-nav

---
+++ responsive nav/cust.js
@@ -0,0 +1,26 @@
+function shownav(){
+    document.getElementById("bars").style.display ="none";
+
+    document.getElementById("times").style.display ="block";
+
+   document.getElementById("mobul").style.display ="block";
+
+
+//  b.style.display ="none";
+//  c.style.display ="block";
+//  m.style.display ="block";
+
+}
+function hide(){
+       document.getElementById("bars").style.display ="block";
+       document.getElementById("times").style.display ="none";
+      document.getElementById("mobul").style.display ="none";
+
+    // b.style.display ="block";
+    // c.style.display ="none";
+    // m.style.display ="none";
+
+    
+
+
+}
